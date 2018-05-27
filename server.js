37a5b2e8d6fd992b1c4adc3f88cd5edb59bfa036
Jeff S <Slater.Jeff7@gmail.com>
2018-05-27 16:12:19 -0600

    server debugging

--- server.js
+++ server.js
@@ -14,28 +14,28 @@ app.use(express.static('public'))
 
 app.get('/api', function(req,res,next){
   res.status(200).send('hello from API')
-}))
+})
 
-app.get('/sauces/', function(req,res,next{
+app.get('/sauces/', function(req,res,next){
   res.sendStatus(200)
-}))
+})
 
-app.get('/sauces/:id', function(req,res,next{
+app.get('/sauces/:id', function(req,res,next){
   res.status(200).send(req.params.id)
-}))
+})
 
-app.post('/sauces', function(req,res,next{
+app.post('/sauces', function(req,res,next){
   res.status(200).send(req.body)
-}))
+})
 
-app.patch('/sauces/:id', function(req,res,next{
+app.patch('/sauces/:id', function(req,res,next){
   let result = { id:req.params.id, name:req.body.name}
   res.status(200).send(result)
-}))
+})
 
-app.delete('/sauces/:id', function(req,res,next{
+app.delete('/sauces/:id', function(req,res,next){
   res.status(200).send(req.params.id)
-}))
+})
 
 app.use(function(req,res,next){
   res.status(404).send({error: {message: "404 Not Found"}})
